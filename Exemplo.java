3729709c15ff134745efae4a5a109201d66f3782
diogo-gb Iwamoto <diogoiwamoto7999@gmail.com>
2023-08-15 19:32:32 +0000

    Primeira aula java2

---
+++ Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo{
+    public static void main(String[] args){
+        Aluno al = new Aluno();
+        al.setNome("Diogo");
+
+        System.out.println(al.getNome());
+    }
+}
